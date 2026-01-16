bbc2b706ca5236c1b0a1c78a0395791023222eb9
Esteban <este8617@gmail.com>
2026-01-16 16:56:31 +0100

    Implémente le SkillRepository et SkillMongoDao Fini d'implémenter le Repository

--- src/main/java/com/imt/Api_monstres/Repository/SkillRepository.java
+++ src/main/java/com/imt/Api_monstres/Repository/SkillRepository.java
@@ -1,10 +1,35 @@
 package com.imt.Api_monstres.Repository;
 
+import com.imt.Api_monstres.Repository.dao.SkillMongoDao;
+import com.imt.Api_monstres.Repository.dto.SkillMongoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class SkillRepository {
+    private final SkillMongoDao skillMongoDao;
 
+    public String save (SkillMongoDto skillMongoDto){
+        SkillMongoDto savedSkillDto = skillMongoDao.save(skillMongoDto);
+        return savedSkillDto.getSkillId();
+    }
+
+    public void delete (String id){
+        skillMongoDao.deleteById(id);
+    }
+
+    public SkillMongoDto findSkillById (String skillId) {
+        return skillMongoDao.findById(skillId).orElse(null);
+    }
+
+    public List<SkillMongoDto> findAllByMonsterId (String monsterId){
+        return skillMongoDao.findAllByMonsterId(monsterId);
+    }
+
+    public void update(SkillMongoDto skillMongoDto) {
+        skillMongoDao.save(skillMongoDto);
+    }
 }
--- src/main/java/com/imt/Api_monstres/Repository/dao/SkillMongoDao.java
+++ src/main/java/com/imt/Api_monstres/Repository/dao/SkillMongoDao.java
@@ -1,5 +1,10 @@
 package com.imt.Api_monstres.Repository.dao;
 
-public class SkillMongoDao {
+import com.imt.Api_monstres.Repository.dto.SkillMongoDto;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
+public interface SkillMongoDao extends MongoRepository <SkillMongoDto, String>{
+    public List<SkillMongoDto> findAllByMonsterId (String id);
 }
